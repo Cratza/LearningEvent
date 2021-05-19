3d1f8573a5f3f7d15c657ad655572287637447b1
浚陶 毛 <1048493621@qq.com>
2021-05-19 15:41:09 +0800

    增加了对比方法

--- 事件练习1-输入值监听-不使用事件实现/Program.cs
+++ 事件练习1-输入值监听-不使用事件实现/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace 事件练习1_输入值监听
+namespace 事件练习1_输入值监听_不使用事件实现
 {
     class Program
     {
@@ -12,8 +12,7 @@ namespace 事件练习1_输入值监听
             Reciever1 reciever1 = new Reciever1();
             Reciever2 reciever2 = new Reciever2();
             // 将监听者的ListenEvent事件与接受者的方法关联（建立订阅关系）
-            listener.ListenEvent += new Action<object, ListenEventArgs>(reciever1.OutPutInf);
-            listener.ListenEvent += new Action<object, ListenEventArgs>(reciever2.OutPutInf);
+
             // 调用ListenInut方法时，由于该方法调用了ListenEvent委托，而该委托被多个具体方法订阅（多播委托）
             // 因此会调用reciever1、reciever2的OutPutInf()方法
             listener.ListenInut();
@@ -37,17 +36,16 @@ namespace 事件练习1_输入值监听
     // 监听类，其方法ListenInut()不断地监听用户的输入值，并将参数传入委托调用
     public class Listener
     {
-        // 声明一个委托，用于订阅事件
-        public event Action<object, ListenEventArgs> ListenEvent;
         public void ListenInut()
         {
             while (true)
             {
                 // 保存输入参数
                 char res = Console.ReadKey().KeyChar;
-                ListenEventArgs e = new ListenEventArgs(res);
-                // 将输入的值作为参数传递到事件中
-                ListenEvent?.Invoke(this, e);
+                Reciever1 reciever1 = new Reciever1();
+                Reciever2 reciever2 = new Reciever2();
+                reciever1.OutPutInf(res);
+                reciever2.OutPutInf(res);
             }
         }
     }
@@ -55,18 +53,18 @@ namespace 事件练习1_输入值监听
     // 接收者类1
     public class Reciever1
     {
-        public void OutPutInf(object sender, ListenEventArgs e)
+        public void OutPutInf(char c)
         {
-            Console.WriteLine($"\n用户输入的值为：{e.inputChar}");
+            Console.WriteLine($"\n用户输入的值为：{c}");
         }
     }
 
     // 接收者类2
     public class Reciever2
     {
-        public void OutPutInf(object sender, ListenEventArgs e)
+        public void OutPutInf(char c)
         {
-            Console.WriteLine($"下个字符为：{(char)(e.inputChar + 1)}");
+            Console.WriteLine($"下个字符为：{(char)(c + 1)}");
         }
     }
 }
